76f5442592f52d17f32548e3380e229e3cf60fe2
Md Ibrahim Arman <160410782+onanimeseries@users.noreply.github.com>
2024-09-15 19:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '@ᴏɴɢᴏɪɴɢ ᴀɴɪᴍᴇ ꜱᴇʀɪᴇꜱ'
-
+    return '@JishuDeveloper'
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to 0.0.0.0 and port 8080 to work with Koyeb
+    app.run(host='0.0.0.0', port=8080)

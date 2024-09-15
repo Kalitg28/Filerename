396888e0e474368b64688eef4da88fdab814a654
Md Ibrahim Arman <160410782+onanimeseries@users.noreply.github.com>
2024-09-15 20:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ API_HASH = os.environ.get("API_HASH", "bdc43a2e0501624d3f0a491345a5ac84")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7133030764:AAE5JAGDqiNigmPiuuJBaC-YhGtijlBv3UQ")
 ADMIN = int(os.environ.get("ADMIN", "5197673717"))
 
+WEB_SUPPORT = bool(os.environ.get("WEB_SUPPORT", "True"))
+
 
 # Premium 4GB Renaming Client Config
 STRING_SESSION = os.environ.get("STRING_SESSION", "BQCPRuQAE4DEstPiqGR-YV3qsRMqgFzM7Q1sIpMULN2i8ytRSIqMIJ05oH-CRKAHeCdaNTnva1mM63wzJUsz5eeAlt66bSKdKuEXFFaaDMrIfhHMx8oduvKHAR2O7NZzkdwjmGeiu3Rks4iguvEhbZutjxdry0zhwTt7Fllx7LPRWyc_4DzWScYmaLABKTG9C356GOnrOn9EahxA6MQalOJdpg4eoti7blTRuU4F8lBJA0yPapaOs3Jm4-cCAQ2ZoAyzIyNAQWqH_8Q5TaJ4-dy9v6NlPZ40n-PugzjinjhWEpa2n0LBZsBCIprrlVccd99dAFbvNRbqoNY1KeDNXaubquwHuwAAAAE1zjT1AA")

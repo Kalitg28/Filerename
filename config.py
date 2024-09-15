b4e91065f70ace87f277b0cd51cbfd8c2a9c753e
Anime Series <onanimeseries@gmail.com>
2024-09-15 18:05:15 +0530

    Added via files

---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+
+
+
+
+# Required Variables Config
+API_ID = int(os.environ.get("API_ID", "9389796"))
+API_HASH = os.environ.get("API_HASH", "bdc43a2e0501624d3f0a491345a5ac84")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7133030764:AAE5JAGDqiNigmPiuuJBaC-YhGtijlBv3UQ")
+ADMIN = int(os.environ.get("ADMIN", "5197673717"))
+
+
+# Premium 4GB Renaming Client Config
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQCPRuQAYq1GNmq1uOhVHtw1XxO0JZXQt0u1b5zpNf3rxvFSzr4N4yKlkrYAu5DhAJE_NQVvM5e3kDmpZggPryNnKslQ-Ltlg6UYkhyGz9dlWy66nOIWUmwEn0I0U2t4quqMWSmQFlCzd5DX63rUGAOb2ThXQSF0h972x8mCvZsPh-fJG2kafph-EbHgdPfy8yDmptZ4Nb5Bt08mlXUfZPe6RCeNGwf7lxbiYXJEmhgq8x7zX8HlKPwLQkB-2VGVwoPwQ1cEeUDsv00kg-vP3-HSuWWUtHsWD47_a-iZ1xBYAbG8SLn-vTovOx2TJ1V9fEi8IoO6ld4tZGqr43ar3TwC9B8rggAAAAE1zjT1AA")
+
+
+# Log & Force Channel Config
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "OnAnimeSeries")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002169251162"))
+
+
+# Mongo DB Database Config
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://OnAnimeSeries:OnAnimeSeries@filexonanimeseries.2lwf4gr.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "FileXOnAnimeSeries")
+
+
+# Other Variables Config
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/d3dde3480b703b3f34e77.jpg")
+
+
+
+
+
+# If You Need To Add Verify System Then Message Me On Telegram
+# Check Demo Bots
+# https://t.me/FileRenameXBot
+# https://t.me/PremiumRenamerRobot
+# https://t.me/FileRenamerXRobot
+# Token Verification Adding Features Is Paid So If You Want Then Dm Me
+
+
+# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
+# SHORTNER_API = os.environ.get("SHORTNER_API", "")
+# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
---
+++ plugins/upgrade.py
@@ -0,0 +1,70 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+from pyrogram import Client , filters
+
+
+
+
+@Client.on_callback_query(filters.regex('upgrade'))
+async def upgrade(bot,update):
+    text = """**Free Plan User**
+Daily  Upload limit 2GB
+Price 0
+
+**🪙 Basic**
+Daily  Upload  limit 20GB
+Price Rs 69  ind /🌎 1$  per Month
+
+**⚡ Standard**
+Daily Upload limit 50GB
+Price Rs 169  ind /🌎 2.19$  per Month
+
+**💎 Pro**
+Daily Upload limit 100GB
+Price Rs 199  ind /🌎 3.16$  per Month
+
+Payment Details :-
+<b>➜ UPI ID :</b> <code>madflixofficial@axl</code>
+<b>➜ PayPal :</b> <a href='https://www.paypal.me/ibbs09'>Click Here</a>
+<b>➜ QR Code :</b> <a href='https://telegra.ph/Scan--Pay-09-15-2'>Click Here</a>
+
+After Payment Send Screenshots Of Payment To Admin @OnAnimeSeriesAdminbot"""
+    
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/OnAnimeSeriesAdminbot"),
+        InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+        ])
+    
+    await update.message.edit(text = text,reply_markup = keybord, disable_web_page_preview=True)
+    
+    
+
+@Client.on_message(filters.private & filters.command(["upgrade"]))
+async def upgradecm(bot,message):
+    text = """**Free Plan User**
+**🪙 Basic**
+Daily  Upload  limit 20GB
+Price Rs 69  ind /🌎 1$  per Month
+
+**⚡ Standard**
+Daily Upload limit 50GB
+Price Rs 169  ind /🌎 2.19$  per Month
+
+**💎 Pro**
+Daily Upload limit 100GB
+Price Rs 199  ind /🌎 3.16$  per Month
+
+Payment Details :-
+<b>➜ UPI ID :</b> <code>madflixofficial@axl</code>
+<b>➜ PayPal :</b> <a href='https://www.paypal.me/ibbs09'>Click Here</a>
+<b>➜ QR Code :</b> <a href='https://telegra.ph/Scan--Pay-09-15-2'>Click Here</a>
+
+After Payment Send Screenshots Of Payment To Admin @OnAnimeSeriesAdminbot"""
+    
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/OnAnimeSeriesAdminbot"),
+        InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
+        ])
+    
+    await message.reply_text(text=text, reply_markup=keybord, quote=True, disable_web_page_preview=True)
+    
+	

9207bdd9d8dad4c514b4c3df1f7bee932246f8dc
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2024-11-06 22:18:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002014126653"))
 
 # Mongo DB Database Config
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://filerename:filerename@cluster0.sd0p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "FileXOnAnimeSeries")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
 # Other Variables Config
